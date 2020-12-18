eae502fee374ba938995a8428ec3c884de82107a
SimonHe <siminghe2020@gmail.com>
2020-12-19 07:53:20 +0800

    aws s3

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,4 +3,4 @@ Aws.config.update({
   credentials: Aws::Credentials.new(Rails.application.credentials.dig(:aws, :access_key_id), Rails.application.credentials.dig(:aws, :secret_access_key)),
 })
 
-S3_BUCKET = Aws::S3::Resource.new.bucket(cis196final)
+S3_BUCKET = Aws::S3::Resource.new.bucket(Rails.application.credentials.dig(:aws, :bucket_name)
